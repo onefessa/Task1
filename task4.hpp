4afde457a39e9e030c34ca82b33f8190001ca12c
onefessa <63928066+onefessa@users.noreply.github.com>
2021-10-03 23:52:03 +0300

    Add files via upload

--- task4.hpp
+++ task4.hpp
@@ -2,33 +2,60 @@
 #include <fstream>
 #include <cmath>
 #include <string>
+
 double y(double t){//аналитическая кривая и наборы точек, полученные методом эйлера
 	return pow(t,2);
 }
-
 void task4 (){
     double h = 0.1;
-    double D = 0;
+    //double D = 0;
     double y_0 = 0;//=0
-    double y_K, d;///K=k+1
+    double y_K;// d; K=k+1
     double t_k = 0;
     std::cout<<"Task 4"<<std::endl;
+    std::ofstream output0("4.txt");
+    std::cout<<"f(0) = "<<f(0)<<std::endl;///f(to)
 ///y_{k+1} = y_k + (t_ {k+1}- t_k)* f(t_k, y_k); y_{1} = y_0 + (t_1 - t_0)* y_0;
     for(int q=1; q <= 3; q++){
         std::string c;
-        c = "4." + to_string(q)+".txt";
+        c = "4a." + to_string(q)+".txt";
         std::ofstream output(c);
         for(int n=0; n < 10/h;n++){
             y_K = y_0 + h*(2*t_k);
-            d = abs(y_K-pow(t_k,2));
-            output << t_k << " " << y_K <<endl;//<< " "  d << std::endl;
+            //d = abs(y_K-pow(t_k,2));
+            output << t_k << " " << y_K;// << " " << d << std::endl;
             y_0=y_K;
             t_k+=h;
-            if (d >= D) {D = d;}
+            //if (d >= D) {D = d;}
             }
-        std::cout << "\tError for step h = "<<h<<": " << D <<std::endl;
+        std::cout<<"A) t_k = "<<t_k<<std::endl;///f(to)
+        std::cout << "\tError for step h = "<<h<<": " << abs(y_K-pow(t_k,2)) <<std::endl;
+        output0<<"Error for step h = "<<h<<": " << abs(y_K-pow(t_k,2)) <<std::endl;
         t_k=0;
         y_0=0;
+        output << std::endl;
+        h/=10.;
+        }
+    h = 0.1;
+    y_0=f(t_k);
+    std::cout<<"t_k = "<<t_k<<"y_0 = "<<y_0<<std::endl;///f(to)
+    for(int q=1; q <= 3; q++){
+        std::string c;
+        c = "4b." + to_string(q)+".txt";
+        std::ofstream output(c);
+        for(int n=0; n < 2.5/h; n++){
+            y_K = y_0 + h*f_1(t_k);
+            //d = abs(y_K-f(t_k));
+            output << t_k << " " << y_K << std::endl;//<< " " << d << std::endl;
+            y_0=y_K;
+            t_k+=h;
+            //if (d >= D) {D = d;}
+            }
+        std::cout<<"B) t_k = "<<t_k<<std::endl;///f(to)
+        std::cout << "\tError for step h = "<<h<<": " << abs(y_K-f(t_k)) <<std::endl;
+        output0<<"Error for step h = "<<h<<": " << abs(y_K-f(t_k)) <<std::endl;
+        t_k=0;
+        y_0=f(t_k);
         output << std::endl;
         h/=10.;
         }
---
+++ task5.hpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+///T = 2 pi, 10 pi, 100 pi, 1 000 000 pi
+///x' = z /  x = sin t / (sin t)' = cos t = z
+///z' = -x / -sin t = -x
+///y_{n+1} = y_n + h f(t_n)
+///______________________________________________________________
+/// |   T   |   h   |   |~x~(T) - x(T)|     |    |~z~(T) - z(T)|
+/// |_______|_______|_______________________|____________________
+/*double x(double t){
+    return sin(t);
+}
+double z(double t){
+    return cos(t);
+}*/
+
+void task5 (){
+    const double pi = 3.141592653589793;
+    double h = 0.1;
+    double x = 0;
+    double z = 1;
+    double x_K, z_K;// d; K=k+1
+    //double t_k = 0;
+    int w = 1;
+    cout<<"Task 5"<<endl;
+    ofstream output0("5.txt");
+    cout<<"T \th\t |~x~(T) - x(T)|  |~z~(T) - z(T)|"<<endl;
+    output0<<"T \th\t |~x~(T) - x(T)| \t |~z~(T) - z(T)|"<<endl;
+    for (int r = 1; r<=6;r++){
+        for(int q=1; q <= 3; q++){
+            string c;
+            c = "5_" +to_string(w)+"pi_"+to_string(h)+".txt";
+            if (w<=100){ofstream output(c);
+            /*
+            for (int n=0; n<=T1; n++){
+                a = x + h*z;
+                b = z - h*x;
+                x = a;
+                z = b;
+                output<<x<<" "<<z<<endl;
+            }
+            */
+            for(int n=0; n < w*pi/h; n++){
+                x_K = x + h*z;
+                z_K = z - h*x;
+                output << x_K<<" "<<z_K<<endl;
+                x=x_K;
+                z=z_K;
+                }
+            }else{
+                for(int n=0; n < w*pi/h; n++){
+                    x_K = x + h*z;
+                    z_K = z - h*x;
+                    x=x_K;
+                    z=z_K;
+                }
+            }
+            cout << w<<"\t "<<h<<"\t " << abs(x_K)<<"\t" << abs(z_K-1)<<endl;
+            output0<<w<<"\\pi & "<<h<<" & " << abs(x_K)<<" & " << abs(z_K-1)<<"\\\\"<<endl;
+            x=0;
+            z=1;
+            h/=10.;
+        }
+        h = 0.1;
+        w*=10;
+    }
+}
