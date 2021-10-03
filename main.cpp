74a8c9cbed28a2af99ec59e5b15327b259d60288
onefessa <63928066+onefessa@users.noreply.github.com>
2021-10-03 23:52:52 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "task2.hpp"
 #include "task3.hpp"
 #include "task4.hpp"
+#include "task5.hpp"
 int main (){
     //task1();
     //task2();
     //task3();
-    task4();
+    //task4();
+    task5();
 return 0;
 }
 
