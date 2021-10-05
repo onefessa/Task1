dc3bf5d7518e8072095c73f85fe5c7b84d6dc6e5
onefessa <63928066+onefessa@users.noreply.github.com>
2021-10-05 09:29:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include "task3.hpp"
 #include "task4.hpp"
 #include "task5.hpp"
+#include "task6.hpp"
 int main (){
     task1();
     task2();
     task3();
     task4();
     task5();
+    task6();    
 return 0;
 }
 
