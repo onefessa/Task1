e614f3b5bd4cdc909353e0e23a0044fa291bd20d
onefessa <63928066+onefessa@users.noreply.github.com>
2021-10-12 04:30:30 +0300

    Add files via upload

--- task6.hpp
+++ task6.hpp
@@ -19,67 +19,85 @@ _____________|________________
 /// y_1      |   35./384        0               500./1113       125./192    -2187./6784     11./84      0
 
 pair <double, double> RKa (double x, double h){
-    ofstream output ("6.txt");
-    double k1, k3, k4, k5, k6, y = 0, y_0 = f(x);
+    ofstream output_a ("6a.txt");
+    double k1, k3, k4, k5, k6, y_0, y = 0;
     for (int i=0; i<1/h; i++){
+        y_0 = f(x);
+
         k1 = f_1(x);
         k3 = f_1(x + (3./10)*h);
         k4 = f_1(x + (4./5)*h);
         k5 = f_1(x + (8./9)*h);
         k6 = f_1(x + (1.)*h);
-        y += y_0 + h*((35./384)*k1 + (500./1113)*k3 + (125./192)*k4 + (-2187./6784)*k5 + (11./84)*k6);
+
+        y = y_0 + h*((35./384)*k1 + (500./1113)*k3 + (125./192)*k4 + (-2187./6784)*k5 + (11./84)*k6);
         x += h;
-        output<< "Pogreshnost' : x = " << x<< ";  y = " << y<<"; f(x) = "<<f(x)<< "; " <<abs(y-f(x)) << endl;
+
+        ///output<< "Err : x = " << x<< ";  y = " << y<<"; f(x) = "<<f(x)<< "; D =" <<abs(y-f(x)) << endl;
+        output_a<< x << "; " << y << endl;
     }
+
     return std::pair<double, double>(x,y);
 }
 
-
 pair<double, double> RKb (double x, double z){
     const double pi = 3.141592653589793;
-    double k1[2], k3[2], k4[2], k5[2], k6[2], x_K, z_K, t = 0,  h = 0.01;
+    double k1,l1,k2,l2, k3,l3,k4,l4,k5,l5, k6,l6, x_K, z_K, t = 0,  h = 0.1;
     ofstream output ("6b_xz.txt");
     ofstream outputp ("6b_pp.txt");
-    for (int i=0; i < 100*pi/h; i++){
-        k1[0] = z;
-        k1[1] = -x;
-        //k2[0] = x + h*(1./5);
-        //k2[1] = -z - h*(1./5);
-        k3[0] = z + h*(3./10);
-        k3[1] = -x - h*(3./10);
-        k4[0] = z + h*(4./5);
-        k4[1] = -x - h*(4./5);
-        k5[0] = z + h*(8./9);
-        k5[1] = -x - h*(8./9);
-        k6[0] = z + h*(1.);
-        k6[1] = -x - h*(1.);
-        //k7[0] = x + h*(1.);
-        //k7[1] = -z - h*(1.);
-        x_K = x + h*((35./384)*k1[0] + (500./1113)*k3[0] + (125./192)*k4[0] + (-2187./6784)*k5[0] + (11./84)*k6[0]);
-        z_K = z + h*((35./384)*k1[1] + (500./1113)*k3[1] + (125./192)*k4[1] + (-2187./6784)*k5[1] + (11./84)*k6[1]);
-        t+=h;
+    for (int i=0; i < 2*pi/h; i++){
+                k1 = z;
+				l1 = -x;
+
+				k2 = (z + (1/5.)*l1);
+				l2 = -(x + (1/5.)*k1);
+
+				k3 = (z +(3/40.)*l1 + (9/40.)*l2);
+				l3 = -(x +(3/40.)*k1 + (9/40.)*k2);
+
+				k4 = (z + (44/45.)*l1 - (56/15.)*l2 + (32/9.)*l3);
+				l4 = -(x + (44/45.)*k1 - (56/15.)*k2 + (32/9.)*k3);
+				k5 = (z + (19372/6561.)*l1 - (25360/2187.)*l2 + (64448/6561.)*l3 - (212/729.)*l4);
+				l5 = -(x + (19372/6561.)*k1 - (25360/2187.)*k2 + (64448/6561.)*k3 - (212/729.)*k4);
+				k6 = (z + (9017/3168.)*l1 - (355/33.)*l2 + (46732/5247.)*l3 +(49/176.)*l4 -(5103/18656.)*l5);
+				l6 = -(x + (9017/3168.)*k1 - (355/33.)*k2 + (46732/5247.)*k3 +(49/176.)*k4 -(5103/18656.)*k5);
+				//k7 = (z + (35/384.)*l1 + (500/1113.)*l3 + (125/192.)*l4 - (2187/6784.)*l5 + (11/84.)*l6);
+				//l7 = -(x + (35/384.)*k1 + (500/1113.)*k3 + (125/192.)*k4 - (2187/6784.)*k5 + (11/84.)*k6);
+
+				x_K = x + h*((35/384.)*k1 + (500/1113.)*k3 + (125/192.)*k4 - (2187/6784.)*k5 + (11/84.)*k6);
+				z_K = z + h*((35/384.)*l1 + (500/1113.)*l3 + (125/192.)*l4 - (2187/6784.)*l5 + (11/84.)*l6);
+				t += h;
+
+        //x_K = x + h*((35./384)*k1[0] + (500./1113)*k3[0] + (125./192)*k4[0] + (-2187./6784)*k5[0] + (11./84)*k6[0]);
+        //z_K = z + h*((35./384)*k1[1] + (500./1113)*k3[1] + (125./192)*k4[1] + (-2187./6784)*k5[1] + (11./84)*k6[1]);
+        //t+=h;
         x=x_K;
         z=z_K;
 ///x' = z   |  x = sin t / (sin t)' = cos t = z
 ///z' = -x  | -sin t = -x
         output << x << " " << z << endl;
-        outputp<<"dx = "<< (sin(t) - x) << ";\t dz = "<< (cos(t) - z) << endl;
+        //outputp<<"dx = "<< (sin(t) - x) << ";\t dz = "<< (cos(t) - z) << endl;
+        outputp<< (sin(t) - x) << "; "<< (cos(t) - z) << endl;
     }
     cout<<"delta x "<< (sin(t) - x) << " : delta z "<< (cos(t) - z) << endl;
     return pair <double, double>(x,z);
 }
 
 void task6(){
-    cout<<"Task 6a\n"<<endl;
+    ofstream output ("6.txt");
 
-    pair <double, double> h1a = RKa(0, 0.1);
-    cout<< "\tx = "<<h1a.first<< ";\t h = 0.1; \t D = "<<abs(f(1) - h1a.second) <<endl;
-    pair <double, double> h2a = RKa(0, 0.01);
-    cout<< "\tx = "<<h2a.first<< ";\t h = 0.01; \t D = "<<abs(f(1) - h2a.second) <<endl;
-    pair <double, double> h3a = RKa(0, 0.001);
-    cout<< "\tx = "<<h3a.first<< ";\t h = 0.001; \t D = "<<abs(f(1) - h3a.second) <<endl;
+    /*cout<<"\nTask 6a\n"<<endl;
+    double h = 1.;
+    pair <double, double> h_a;
+	for(int i=1;i<=5;i++){
+		h/=10;
+		h_a = RKa(0, h);
+		cout<< "\tx = "<<h_a.first<< ";\t h = "<<h<<"; \t D = "<<abs(f(1) - h_a.second) <<endl;
+		output<< "\tx = " << h_a.first << ";\t h = " << h << "; \t D = " << abs(f(1) - h_a.second) <<endl;
+	}*/
 
     cout<<"Task 6b\n"<<endl;
     pair <double, double> h1b = RKb(0, 1);
-    cout<< "h = 0.001;\tx = "<<h1b.first<<"\tz = "<<h1b.second<<endl;
+    cout<< "h = 0.001:\tx = "<<h1b.first<<"\tz = "<<h1b.second<<endl;
+    output<<"h = 0.001:\tx = "<<h1b.first<<"\tz = "<<h1b.second<<endl;
 }
